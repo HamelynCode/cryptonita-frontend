ff0a92779a39a12264cb38c3a59202a4fc24931c
EduardoRodriguez <rodriguezalcuadrado@proton.me>
2023-03-20 03:50:09 -0300

    add preloader component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,10 +2,12 @@ import './Main.css';
 import About from '../About/About';
 import Footer from '../Footer/Footer';
 import MainButton from '../MainButton/MainButton';
+import Preloader from '../Preloader/Preloader';
 
 export default function Main(props){
   return(
   <main className='main'>
+    <Preloader/>
     <section className='main__body'>
       <h1 className='main__title'>CryptoNita</h1>
       <p className='main__text'>Visualiza los precios de las crypto divisas más populares del mercado.</p>
--- src/components/Preloader/Preloader.js
+++ src/components/Preloader/Preloader.js
@@ -2,8 +2,10 @@ import './Preloader.css';
 
 export default function Preloader(){
   return(
-  <>
-    <h2>Preloader</h2>
-  </>
+  <div className='preloader'>
+    <div className='preloader__container'>
+      <div className='preloader__symbol'></div>
+    </div>
+  </div>
   );
 };
